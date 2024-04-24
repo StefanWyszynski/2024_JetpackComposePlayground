6b639afaf8c2adfed59e9afe975a197cc1f663fb
StefanWyszynski <m3gdev@gmail.com>
2024-04-24 08:43:28 +0200

    added main dispatcher to DI

--- app/src/main/java/com/jetpackcompose/playground/di/modules/DispathersModule.kt
+++ app/src/main/java/com/jetpackcompose/playground/di/modules/DispathersModule.kt
@@ -1,25 +1,14 @@
 package com.jetpackcompose.playground.di.modules
 
-import android.util.Log
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonParser
-import com.google.gson.JsonSyntaxException
 import com.jetpackcompose.playground.di.annotations.DispathersDefault
 import com.jetpackcompose.playground.di.annotations.DispathersIO
-import com.jetpackcompose.playground.main.data.api.GitHubApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
-
 
 /*
  * Copyright 2024
@@ -42,6 +31,13 @@ class DispathersModule {
     @DispathersDefault
     fun provideDefaultDispatcher(): CoroutineDispatcher {
         return Dispatchers.Default
+    }
+
+    @Provides
+    @Singleton
+    @DispathersDefault
+    fun provideMainDispatcher(): CoroutineDispatcher {
+        return Dispatchers.Main
     }
 }
 
